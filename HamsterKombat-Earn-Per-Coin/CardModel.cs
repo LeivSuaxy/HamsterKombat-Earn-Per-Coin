94073136a3d13975e7e28f1076ac0abf6fe6e039
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-07-23 03:24:03 -0700

    Add project files.

---
+++ HamsterKombat-Earn-Per-Coin/CardModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterKombat_Earn_Per_Coin
+{
+    internal class CardModel
+    {
+        uint id;
+        string name;
+        double price;
+        double earn_per_hour;
+
+        public CardModel(uint id, string name, double price, double earn_per_hour)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.earn_per_hour = earn_per_hour;
+        }
+
+        public uint GetId() => id;
+
+        public string GetName() => name;
+
+        public double GetPrice() => price;
+
+        public double GetEarn_per_hour() => earn_per_hour;
+
+        public double GetEarn_per_coint() => (earn_per_hour / price);
+
+        public void SetPrice(double price) { this.price = price; }
+
+        public void SetEarn_per_hour(double earn_per_hour) { this.earn_per_hour = earn_per_hour; }
+
+
+    }
+}
---
+++ HamsterKombat-Earn-Per-Coin/Controller.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace HamsterKombat_Earn_Per_Coin
+{
+    internal class Controller
+    {
+        private List<CardModel> cards;
+
+        public Controller()
+        {
+            cards = new List<CardModel>();
+            this.InitDatabaseProccess();
+
+        }
+
+        #region DATABASES
+        private void InitDatabaseProccess()
+        {
+            string dbname = "cards.sqlite";
+
+            if (!this.ExistsDB(dbname))
+            {
+                CreateDatabase(dbname);
+            }
+
+            LoadCards(dbname);
+
+        }
+
+        private bool ExistsDB(string dbname)
+        {
+            if (File.Exists(dbname))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        
+        private void CreateDatabase(string dbname)
+        {
+            SQLiteConnection.CreateFile(dbname);
+
+            using (var connection = new SQLiteConnection(this.GetConnectionString(dbname)))
+            {
+                connection.Open();
+
+                string query =  "CREATE TABLE IF NOT EXISTS cards (" +
+                                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "name TEXT," +
+                                "price DOUBLE," +
+                                "gain DOUBLE" +
+                                ");";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void LoadCards(string dbname)
+        {
+            using (var connection = new SQLiteConnection(this.GetConnectionString(dbname)))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM cards ORDER BY id";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            CardModel card = new CardModel(
+                                id: Convert.ToUInt32(reader["id"]),
+                                name: reader["name"].ToString(),
+                                price: Convert.ToDouble(reader["price"]),
+                                earn_per_hour: Convert.ToDouble(reader["gain"])
+                            );
+
+                            cards.Add(card);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        private string GetConnectionString(string dbname) => $"Data Source={dbname};Version=3;";
+
+        #endregion
+        #region CRUD
+
+        #endregion
+    }
+}
